a89c78419eb47b301f754ce4d541ab17ea5d0330
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2013-07-06 23:12:34 +0200

    Cleanup.

--- src/test/java/integration/database/DataSourceIntegrationTest.java
+++ src/test/java/integration/database/DataSourceIntegrationTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import javax.sql.DataSource;
 
@@ -12,19 +12,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @ContextConfiguration(locations = {"/META-INF/spring/applicationContext.xml"})
-public class DataSourceIntegrationTest extends AbstractJUnit4SpringContextTests {
+public class DataSourceIntegrationTest extends AbstractTransactionalJUnit4SpringContextTests {
 
     @Autowired
     private DataSource dataSource;
 
     @Test
-    public void dataSource_notNull() throws Exception {
+    public void notNull() throws Exception {
         assertNotNull(dataSource);
     }
 
     @Test
     public void select() throws Exception {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        assertEquals(0, jdbcTemplate.queryForList("select 1 from categories", Integer.class).size());
+        jdbcTemplate.update("DELETE FROM categories");
+        assertEquals(0, jdbcTemplate.queryForList("SELECT 1 FROM categories", Integer.class).size());
     }
+
 }
